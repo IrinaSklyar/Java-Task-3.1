a9792e869a38d50bcbf17a262313d720afde6a8e
Irina Skliarova <i.skliarova2015@yandex.ru>
2020-04-12 09:50:59 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 120;
+        int bonus = 1;
+        int totalBonus = (amount/20);
+                  System.out.println (totalBonus);
+    }
+}
