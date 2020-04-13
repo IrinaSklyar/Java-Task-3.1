2e13671b458e2943bada07362321cb6f4ca3a6bd
Irina Skliarova <i.skliarova2015@yandex.ru>
2020-04-13 18:00:53 +0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int amount = 120;
-        int bonus = 1;
-        int totalBonus = (amount/20);
-                  System.out.println (totalBonus);
+        int totalBonus = (amount / 20);
+        System.out.println(totalBonus);
     }
 }
